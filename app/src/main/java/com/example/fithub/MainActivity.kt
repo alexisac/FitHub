08057c97b4a10269dd23da8224f544001cad74ad
AlexIsac <alex_isac72@yahoo.com>
2026-07-13 21:33:56 +0300

    Initial project setup

--- app/src/main/java/com/example/fithub/MainActivity.kt
+++ app/src/main/java/com/example/fithub/MainActivity.kt
@@ -16,7 +16,6 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            //
             FitHubRoot {
                 AppNavHost()
             }
